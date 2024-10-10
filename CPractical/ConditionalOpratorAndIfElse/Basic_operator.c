442bae0d7737d1abc6d6273dfdeaaf3f28694584
poojaapatil2117 <poojalpatil21@gmail.com>
2024-10-10 16:46:19 +0530

    C Practical Work

---
+++ CPractical/ConditionalOpratorAndIfElse/Basic_operator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the two number %d%d",&a,&b);
+
+    if(a>b){
+        printf("the number %d is grater",a);
+    }
+
+    return 0;
+
+}
