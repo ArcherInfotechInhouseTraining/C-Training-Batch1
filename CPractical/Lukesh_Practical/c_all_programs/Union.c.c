42318113398834c9e044c72513115bd255acdc33
lukeshsable <sablelukesh558@gmail.com>
2024-10-15 11:17:44 +0530

    all c programs uploaded

---
+++ CPractical/Lukesh_Practical/c_all_programs/Union.c.c
@@ -0,0 +1,202 @@
+/************************************************************** UNION IN C **********************************************************************
+	It is also user-defined data type like a structure. it may hold elements of any
+	type and any count. It is just a templet,used to allocate the memory for its variables.
+
+		The memory allocated for the variable of union is equals to the highest memory
+	holder element enclosed within the union templet.
+*/
+
+//#include<stdio.h>
+//union Test
+//{
+//	int x;
+//	float y;
+//	char ch;
+//	double db;
+//};
+//
+//int main()
+//{
+//	union Test t;
+//
+//	printf("\n Size of Test is: %d",sizeof( union Test)); //sizeof(t)
+//
+//	return 0;
+//}
+//==============================================================================================================================================================
+//#include<stdio.h>
+
+//union test{
+//
+//int x;
+//float y;
+//char ch;
+//double db;
+//
+//};
+//
+//  int main()
+//{
+//    union test t;
+//
+//    printf("\n Size of Test is: %d",sizeof( union test));
+//
+//    printf("\n Enter The Value of int :");
+//    scanf("%d",&t.x);
+//    printf("\n Value Of int is %d",t.x);
+//
+//    printf("\n Enter The Character Value :");
+//    scanf("%c",&t.ch);
+//    printf("\n Value Of Character is %c",t.ch);
+//
+//    printf("\n Enter The Float Value :");
+//    scanf("%f",&t.y);
+//    printf("\n Value Of Float is %f",t.y);
+//
+//    printf("\n Enter The Value Double :");
+//    scanf("lf",&t.db);
+//    printf("\n Value Of Double is :%lf",t.db);
+//
+//  return 0;
+//}
+//==============================================================================================================================================================
+//#include<stdio.h>
+//union Test
+//{
+//	int x;
+//	float y;
+//	char ch;
+//	double db;
+//};
+
+//int main()
+//{
+//	union Test t;
+//	printf("\n Size of Test is: %d",sizeof( union Test));
+//
+//	printf("\n Enter the character value: ");
+//	scanf("%c",&t.ch);
+//	printf("\n character is %c",t.ch);
+//
+//	printf("\n Enter the int value: ");
+//	scanf("%d",&t.x);
+//	printf("\n int is %d",t.x);
+//
+//	printf("\n Enter the float value: ");
+//	scanf("%f",&t.y);
+//	printf("\n float is %f",t.y);
+//
+//	printf("\n Enter the double value: ");
+//	scanf("%lf",&t.db);
+//	printf("\n Double is %lf",t.db);
+//
+//	return 0;
+//}
+//==============================================================================================================================================================
+//#include<stdio.h>
+//
+//union Test
+//{
+//	int x;
+//	float y;
+//	char ch;
+//	double db;
+//};
+//
+//int main()
+//{
+//	union Test t;
+//	printf("\n Size of Test is: %d",sizeof( union Test));
+//
+//	printf("\n Enter the character value: ");
+//	scanf("%c",&t.ch);
+//
+//	printf("\n Enter the int value: ");
+//	scanf("%d",&t.x);
+//
+//	printf("\n Enter the float value: ");
+//	scanf("%f",&t.y);
+//
+//	printf("\n Enter the double value: ");
+//	scanf("%lf",&t.db);
+//
+//    printf("\n character is %c",t.ch);
+//    printf("\n int is %d",t.x);
+//    printf("\n float is %f",t.y);
+//	printf("\n double is %lf",t.db);
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//enum color
+//{
+//    RED = 0,
+//    GREEN = 1,
+//    BLUE = 2,
+//};
+//int main()
+//{
+//    enum color my_color = RED;
+//    enum color my_color1 = 0;
+//
+//    if (my_color1 == RED)
+//    {
+//        printf("The color is red.\n");
+//    }
+//       else if (my_color1 == BLUE)
+//    {
+//        printf("The color is blue.\n");
+//    }
+//    else if (my_color1 == GREEN)
+//    {
+//        printf("The color is green.\n");
+//    }
+//
+//    else
+//    {
+//        printf("Invalid color!\n");
+//    }
+//    return 0;
+//}
+//==============================================================================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
